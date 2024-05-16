8360e10bc742e757770b734c24119918e0d72828
boterop <boterop22@gmail.com>
2024-05-15 21:22:14 -0500

    Update version in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="html2object",
-    version="0.1.4",
+    version="1.0.0",
     author="boterop",
     author_email="boterop22@gmail.com",
     description="Tools to handle the CRUD of .html files as objects.",
